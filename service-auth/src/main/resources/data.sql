61fe4ae91dfbe8a6825b7e2fab162b5ab36dbf23
JefHerc <jeferson.herculano@outlook.com>
2025-10-06 22:19:54 -0300

    #8 fix: Adição campos e role Paciente

--- service-auth/src/main/resources/data.sql
+++ service-auth/src/main/resources/data.sql
@@ -1,5 +1,14 @@
-insert into public.role (name) values ('ADMIN');
-insert into public.role (name) values ('MEDICO');
-insert into public.role (name) values ('ENFERMERO');
+insert into public.role (id, name) values (0, 'ADMIN');
+insert into public.role (id, name) values (1, 'MEDICO');
+insert into public.role (id, name) values (2, 'ENFERMERO');
+insert into public.role (id, name) values (3, 'PACIENTE');
 
-insert into public.usuario (active, role_id, password, username) values (true, 1, '$2a$10$le9yY1g8ilNcr.OdtqVspOrJR6XrdFxeZbonrdZAmhq40LeGRi0XO', 'admin');
+insert into public.usuario (active, role_id, password, username, name, email)
+values (
+  true,
+  (select id from public.role where name = 'ADMIN'),
+  '$2a$10$le9yY1g81lNcr.OdtQsPr0JRxdfXezbnondrZAhq4eL6R1exo',
+  'admin',
+  'Administrador do Sistema',
+  'adm@teste.com'
+);
