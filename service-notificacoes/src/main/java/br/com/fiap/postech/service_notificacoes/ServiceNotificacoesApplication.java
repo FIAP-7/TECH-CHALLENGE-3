05638bd770f572b49ffbe2f17249ed0ac4f45d84
Felipe Matos <Felipe_Matos@Hotmail.com.br>
2025-10-07 02:05:51 -0300

    #7 fix: Adição de anotação no ServiceNotificacoesApplication

--- service-notificacoes/src/main/java/br/com/fiap/postech/service_notificacoes/ServiceNotificacoesApplication.java
+++ service-notificacoes/src/main/java/br/com/fiap/postech/service_notificacoes/ServiceNotificacoesApplication.java
@@ -2,8 +2,10 @@ package br.com.fiap.postech.service_notificacoes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ServiceNotificacoesApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceNotificacoesApplication.class, args);
